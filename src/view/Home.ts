2dc40744eb0de5ed8b5a8000e25b064852a8a5f0
dilrong <dilrong@dilrong.com>
2021-11-21 22:49:52 +0900

    setup

--- src/view/Home.ts
+++ src/view/Home.ts
@@ -7,14 +7,7 @@ export default class Home implements View {
     private interval: any;
 
     constructor() {
-        this.container = el(".home-view",
-            el("h2", "책과 세계관 소개"),
-            el("h2", "커뮤니티"),
-            el("h2", "크레이터"),
-            el("h2", "인플루엔서"),
-            el("h2", "콜렉터"),
-            el("h2", "데브팀"),
-            el("h2", "로드맵")).appendTo(BodyNode);
+        this.container = el(".home-view").appendTo(BodyNode);
     }
 
     public changeParams(params: ViewParams, uri: string): void { }
