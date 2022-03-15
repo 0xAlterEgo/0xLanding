191a164a75e0bd2edd962ccfc1cd760ff009149f
dilrong <dilrong@dilrong.com>
2022-03-15 16:28:04 +0900

    fix: autoplay

--- src/view/Home.ts
+++ src/view/Home.ts
@@ -6,16 +6,23 @@ export default class Home implements View {
 
     private container: DomNode;
 
+    private video: DomNode<HTMLVideoElement>;
+
     constructor() {
         Layout.current.content.append(
             this.container = el(".home-view",
                 el(".video-container",
-                    el("video", { "autoplay": "", "loop": "" },
+                    this.video = el("video", { "autoplay": "", "loop": "", "muted": "" },
                         el("source", { src: "/video/home.mp4", type: "video/mp4" })
                     ),
                 ),
             ).appendTo(BodyNode)
         );
+        this.init()
+    }
+
+    private init() {
+        this.video.domElement.muted = true;
     }
 
     public changeParams(params: ViewParams, uri: string): void { }
