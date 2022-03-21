1293ac480e7a117af7f2a1a224387f70c5d43976
dilrong <dilrong@dilrong.com>
2022-03-21 18:22:03 +0900

    feat: checkDiscordLogin

--- src/view/Home.ts
+++ src/view/Home.ts
@@ -1,10 +1,15 @@
 import { BodyNode, DomNode, el } from "@hanul/skynode";
 import { View, ViewParams } from "skyrouter";
+import superagent from "superagent";
+import DiscordUserInfo from "../DiscordUserInfo";
+import EthereumWallet from "../ethereum/EthereumWallet";
 import Layout from "./Layout";
 
 export default class Home implements View {
 
     private container: DomNode;
+
+    public discordUser: DiscordUserInfo | undefined;
 
     private video: DomNode<HTMLVideoElement>;
     private audio: DomNode<HTMLAudioElement>;
@@ -23,23 +28,23 @@ export default class Home implements View {
                         el("source", { src: "/video/home.mp4", type: "video/mp4" })
                     ),
                 ),
-                el(".audio-container", {
-                    click: () => {
-                        if (this.isPlay) {
-                            this.audio.domElement.pause();
-                            this.isPlay = false;
-                            this.playButton.deleteClass("playing");
-                        } else {
-                            this.audio.domElement.play();
-                            this.isPlay = true;
-                            this.playButton.addClass("playing");
-                        }
-                    }
-                },
+                el(".audio-container",
                     el("a", { href: "https://opensea.io/collection/penguins-dwvim00c90", target: "_blank" },
                         el("img", { src: "/images/view/home/player.png", alt: "player" })
                     ),
-                    el(".play-container",
+                    el(".play-container", {
+                        click: () => {
+                            if (this.isPlay) {
+                                this.audio.domElement.pause();
+                                this.isPlay = false;
+                                this.playButton.deleteClass("playing");
+                            } else {
+                                this.audio.domElement.play();
+                                this.isPlay = true;
+                                this.playButton.addClass("playing");
+                            }
+                        }
+                    },
                         this.playButton = el(".play-button"),
                         this.audio = el("audio", { "controls": "", "loop": "", },
                             el("source", { src: "/video/bgm-loop.mp3", type: "audio/mp3" }),
@@ -55,18 +60,77 @@ export default class Home implements View {
                     }),
                     this.walletDialog = el(".wallet-dialog", "Please connect your wallet and sign\nthis transaction to access the bot \nservices provided by 0xAlterEgo...!", {
                         click: () => {
-
+                            window.open("https://discord.com/api/oauth2/authorize?client_id=939799839129096244&redirect_uri=https%3A%2F%2Fapp.0xalterego.com%2Flink-wallet-to-discord&response_type=code&scope=identify");
                         },
                     }, el("a", "CONNECT WALLET"),
                     ),
                 ),
             ).appendTo(BodyNode)
         );
-        this.init()
+        this.init();
     }
 
     private init() {
         this.video.domElement.muted = true;
+        this.checkDiscordLogin();
+    }
+
+    private async checkDiscordLogin() {
+
+        let code: string | undefined = new URLSearchParams(window.location.search).get("code")!;
+        if (code !== null) {
+            try {
+                await superagent.get("https://api.0xalterego.com/discord/token").query({
+                    code,
+                    redirect_uri: `${window.location.protocol}//${window.location.host}/link-wallet-to-discord`,
+                });
+            } catch (error) {
+                console.error(error);
+                code = undefined;
+            }
+        } else {
+            code = undefined;
+        }
+
+        if (code !== undefined) {
+            try {
+                const result = await superagent.get("https://api.0xalterego.com/discord/me").query({ code });
+                this.discordUser = result.body;
+                this.checkWalletConnected(code);
+            } catch (error) {
+                console.error(error);
+            }
+        }
+    }
+
+    private async checkWalletConnected(code: string) {
+        if (await EthereumWallet.connected() !== true) {
+            await EthereumWallet.connect();
+        }
+        const address = await EthereumWallet.loadAddress();
+        if (address !== undefined) {
+
+            const message = "Link Wallet to Discord";
+            const signedMessage = await EthereumWallet.signMessage(message);
+
+            try {
+                const result = await fetch("https://api.0xalterego.com/link-wallet-to-discord", {
+                    method: "POST",
+                    body: JSON.stringify({
+                        code,
+                        signedMessage,
+                        address,
+                    }),
+                });
+                if ((await result.json()).linked === true) {
+                    alert("Link Succeed.");
+                } else {
+                    alert("Link Failed.");
+                }
+            } catch (error) {
+                console.error(error);
+            }
+        }
     }
 
     public changeParams(params: ViewParams, uri: string): void { }
